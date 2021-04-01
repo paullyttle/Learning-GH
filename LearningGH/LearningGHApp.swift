0fdc7347e981a84c2003d4187558f0e22c64bfae
Paul Lyttle <p.lyttle@ntlworld.com>
2021-04-01 21:22:59 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Paul Lyttle on 01/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
